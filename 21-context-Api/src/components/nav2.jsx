a539436717e47294a5e1648d528539ca0d1dd8f1
fenil viroja <fenilviroja497@gmail.com>
2026-08-16 17:15:58 +0530

    21-context-Api done

---
+++ 21-context-Api/src/components/nav2.jsx
@@ -0,0 +1,12 @@
+const Nav2 = () => {
+  return (
+    <div className="nav-2">
+      <h4>Home</h4>
+      <h4>About</h4>
+      <h4>Contact</h4>
+      <h4>Services</h4>
+    </div>
+  );
+};
+
+export default Nav2;
---
+++ 21-context-Api/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import Nav2 from "./nav2";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="nav">
+        <h2>Fenil</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
